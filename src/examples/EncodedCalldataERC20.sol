56fdb91385d91b5c5194a2369c98247770af941f
Sunur Efe Vural <efe@efe.kim>
2023-11-24 14:38:35 +0300

    add example encoded contract

---
+++ src/examples/EncodedCalldataERC20.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {ERC20} from "@openzeppelin/token/ERC20/ERC20.sol";
+import {SafeCast} from "@openzeppelin/utils/math/SafeCast.sol";
+import {LEB128Lib} from "../LEB128Lib.sol";
+
+/// @notice Example of an ERC20 token using LEB128 encoded calldata for `approve` and `transfer`.
+/// @author shung (https://github.com/Shungy)
+contract EncodedCalldataERC20 is ERC20("Token", "TKN") {
+    using LEB128Lib for uint256;
+    using SafeCast for uint256;
+
+    function alternativeSelector(bytes4 selector) external pure returns (bytes memory) {
+        if (selector == ERC20.approve.selector) return hex"00";
+        else if (selector == ERC20.transfer.selector) return hex"01";
+        else return hex""; // Means no alternative exists.
+    }
+
+    function alternativeEncoding(bytes calldata selector) external pure returns(string memory) {
+        if (selector.length == 1 && (selector[0] == 0x00 || selector[0] == 0x01))
+            return "leb128-nooffset";
+        return "solidity-abi";
+    }
+
+    fallback(bytes calldata data) external returns (bytes memory) {
+        uint256 to; uint256 amount; uint256 ptr = 1;
+        (to, ptr) = ptr.rawDecodeUint();
+        (amount, ptr) = ptr.rawDecodeUint();
+        if (ptr > data.length) revert();
+
+        if (data[0] == 0x00) return abi.encode(approve(address(to.toUint160()), amount));
+        else if (data[0] == 0x01) return abi.encode(transfer(address(to.toUint160()), amount));
+        else revert();
+    }
+}
---
+++ test/examples/EncodedCalldataERC20.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {LEB128Lib} from "../../src/LEB128Lib.sol";
+import {EncodedCalldataERC20} from "../../src/examples/EncodedCalldataERC20.sol";
+
+contract EncodedCalldataERC20Test is Test {
+    EncodedCalldataERC20 public token;
+
+    function setUp() public {
+        token = new EncodedCalldataERC20();
+
+        deal(address(token), address(this), type(uint256).max);
+    }
+
+    function testApprove(address to, uint256 amount) public {
+        vm.assume(to != address(this));
+        vm.assume(to != address(0));
+
+        bytes memory data = abi.encodePacked(
+            hex"00",
+            LEB128Lib.encode(uint160(to)),
+            LEB128Lib.encode(amount)
+        );
+
+        (bool result, bytes memory returnData) = address(token).call{ value: 0 }(data);
+        assertTrue(result);
+        assertTrue(abi.decode(returnData, (bool)));
+
+        assertEq(token.allowance(address(this), to), amount);
+    }
+
+    function testTransfer(address to, uint256 amount) public {
+        vm.assume(to != address(this));
+        vm.assume(to != address(0));
+
+        bytes memory data = abi.encodePacked(
+            hex"01",
+            LEB128Lib.encode(uint160(to)),
+            LEB128Lib.encode(amount)
+        );
+
+        (bool result, bytes memory returnData) = address(token).call{ value: 0 }(data);
+        assertTrue(result);
+        assertTrue(abi.decode(returnData, (bool)));
+
+        assertEq(token.balanceOf(to), amount);
+        assertEq(token.balanceOf(address(this)), type(uint256).max - amount);
+    }
+}
